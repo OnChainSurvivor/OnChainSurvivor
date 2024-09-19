f14e7e10eebb6ac67b78b0f98b468a5132c5fbf9
OnChain Survivor <172989475+OnChainSurvivor@users.noreply.github.com>
2024-09-18 23:00:29 -0400

    PWA reimplementationtests

--- Deprecated/PWA.js
+++ Deprecated/PWA.js
@@ -14,15 +14,6 @@ if ('serviceWorker' in navigator) {
   }  
 
 /*---------------------------------------------------------------------------
-                            Add this to the index.html header
-
-  <link rel="manifest" href="/manifest.json">
-  <meta name="apple-mobile-web-app-capable" content="yes">
-  <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
-  <meta name="apple-mobile-web-app-title" content="Onchain Survivor">
-  <link rel="apple-touch-icon" href="Media/Classes/Onchain Survivor/MSURVIVOR.png">
-
-
                             Add this to the index.html body
 
    <script src="service-worker.js"></script>
