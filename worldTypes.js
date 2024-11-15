6f65e6bc878a116911ae0b7b202ba6bf5da02c97
OnChain Survivor <172989475+OnChainSurvivor@users.noreply.github.com>
2024-11-15 09:16:16 -0500

    Cleaning

---
+++ worldTypes.js
@@ -0,0 +1,12 @@
+export const abilityTypes = [
+    {title: "The Dark Forest",
+        description:'Survive in Ethereum, an open, futuristic landscape where data flows freely. Be aware of whats lurking in the dark!',
+        thumbnail: 'Media/Worlds/ETHEREUMVERSE.png',
+        effect: [],
+    }, 
+    {title: "Digital Goldland",
+        description:'Outlast 1000 Survivors in the Bitcoin world, where everything gleams in easily gained (and lost) virtual gold.',
+        thumbnail: 'Media/Worlds/GOLDLAND.png',
+        effect: [],
+    },
+];
