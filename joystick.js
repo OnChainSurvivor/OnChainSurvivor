c10b47b8acdfb5a8931e093e52221fd545cea9aa
OnChain Survivor <172989475+OnChainSurvivor@users.noreply.github.com>
2025-02-12 14:27:07 -0500

    Sensitivity fix

--- joystick.js
+++ joystick.js
@@ -89,7 +89,7 @@ function deactivateJoystick() {
 function updateJoystickDirection(normalizedX, normalizedY) {
     keys.w = keys.a = keys.s = keys.d = false;
 
-    const sensitivity = 40 ;
+    const sensitivity = 1 ;
 
     const adjustedX = normalizedX * sensitivity;
     const adjustedY = normalizedY * sensitivity;
