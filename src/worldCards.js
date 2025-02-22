c7da2e543f3eca69cfafe7160dc95fe8351b9eaa
OnChain Survivor <172989475+OnChainSurvivor@users.noreply.github.com>
2025-02-22 09:44:08 -0500

    re added digital goldland

--- src/worldCards.js
+++ src/worldCards.js
@@ -2,4 +2,8 @@ export const worldTypes = [
 {title: 'The Dark Forest',
     description:'Survive in Ethereum, an open, futuristic landscape where data flows freely. Be aware of whats lurking in the dark!',
     thumbnail: 'Media/Worlds/ETHEREUMVERSE.png',
+},
+{title: 'Digital Goldland',
+    description:'Outlast 1000 Survivors in the Bitcoin world, where everything gleams in easily gained (and lost) Digital Gold.',
+    thumbnail: 'Media/Worlds/ETHEREUMVERSE.png',
 }];
